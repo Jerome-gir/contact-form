47a6d7600014ccac760236d036213e2be3f0cec5
Jerome <jerome.gir@gmail.com>
2024-10-14 12:53:17 +0200

    add success toast

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -16,6 +16,8 @@ import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Checkbox } from "@/components/ui/checkbox"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 const formSchema = z.object({
   firstName: z.string().min(2, {
@@ -52,154 +54,186 @@ export default function ContactForm() {
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values)
-    // Here you would typically send the form data to your server
+    // Afficher un toast de succès
+    toast.success(
+      <div>
+        <p className="text-lg font-bold">Message Sent!</p>
+        <p className="text-sm">
+          {"Thanks for completing the form. We'll be in touch soon!"}
+        </p>
+      </div>,
+      {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      }
+    )
+    form.reset()
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-        <FormField
-          control={form.control}
-          name="firstName"
-          render={({ field, fieldState }) => (
-            <FormItem>
-              <FormLabel className="text-black">First Name *</FormLabel>
-              <FormControl>
-                <Input
-                  {...field}
-                  className={fieldState.invalid ? "border-red" : ""}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="lastName"
-          render={({ field, fieldState }) => (
-            <FormItem>
-              <FormLabel className="text-black">Last Name *</FormLabel>
-              <FormControl>
-                <Input
-                  {...field}
-                  className={fieldState.invalid ? "border-red" : ""}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field, fieldState }) => (
-            <FormItem>
-              <FormLabel className="text-black">Email Address *</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="johndoe@example.com"
-                  {...field}
-                  className={fieldState.invalid ? "border-red" : ""}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="queryType"
-          render={({ field, fieldState }) => (
-            <FormItem>
-              <FormLabel className="text-black">Query Type *</FormLabel>
-              <FormControl>
-                <RadioGroup
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                  className="flex flex-col space-y-1"
-                >
-                  <div
-                    className={`border rounded-md p-2 ${
-                      fieldState.invalid ? "border-red" : ""
-                    }`}
-                  >
-                    <FormItem className="flex items-center space-x-3 space-y-0">
-                      <FormControl>
-                        <RadioGroupItem value="General Enquiry" />
-                      </FormControl>
-                      <FormLabel className="font-normal text-black">
-                        General Enquiry
-                      </FormLabel>
-                    </FormItem>
-                  </div>
-                  <div
-                    className={`border rounded-md p-2 mt-2 ${
-                      fieldState.invalid ? "border-red" : ""
-                    }`}
-                  >
-                    <FormItem className="flex items-center space-x-3 space-y-0">
-                      <FormControl>
-                        <RadioGroupItem value="Support Request" />
-                      </FormControl>
-                      <FormLabel className="font-normal text-black">
-                        Support Request
-                      </FormLabel>
-                    </FormItem>
-                  </div>
-                </RadioGroup>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="message"
-          render={({ field, fieldState }) => (
-            <FormItem>
-              <FormLabel className="text-black">Message *</FormLabel>
-              <FormControl>
-                <Textarea
-                  placeholder="Type your message here."
-                  className={`resize-none h-52 ${
-                    fieldState.invalid ? "border-red" : ""
-                  }`}
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="consent"
-          render={({ field }) => (
-            <FormItem className="flex flex-row items-start space-x-3 space-y-0">
-              <FormControl>
-                <Checkbox
-                  checked={field.value}
-                  onCheckedChange={field.onChange}
-                />
-              </FormControl>
-
-              <div className="space-y-1 leading-none">
-                <FormLabel className="text-black">
-                  I consent to being contacted by the team *
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="firstName"
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel className="text-black text-base cursor-pointer">
+                  First Name *
                 </FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    className={fieldState.invalid ? "border-red" : ""}
+                  />
+                </FormControl>
                 <FormMessage />
-              </div>
-            </FormItem>
-          )}
-        />
-        <Button
-          type="submit"
-          className="w-full bg-green-600 hover:bg-green-700"
-        >
-          Submit
-        </Button>
-      </form>
-    </Form>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lastName"
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel className="text-black text-base cursor-pointer">
+                  Last Name *
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    className={fieldState.invalid ? "border-red" : ""}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel className="text-black text-base cursor-pointer">
+                  Email Address *
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="johndoe@example.com"
+                    {...field}
+                    className={fieldState.invalid ? "border-red" : ""}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="queryType"
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel className="text-black text-base">
+                  Query Type *
+                </FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex flex-col space-y-1"
+                  >
+                    <div
+                      className={`border rounded-md p-2 ${
+                        fieldState.invalid ? "border-red" : ""
+                      }`}
+                    >
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="General Enquiry" />
+                        </FormControl>
+                        <FormLabel className="font-normal text-black text-base cursor-pointer">
+                          General Enquiry
+                        </FormLabel>
+                      </FormItem>
+                    </div>
+                    <div
+                      className={`border rounded-md p-2 mt-2 ${
+                        fieldState.invalid ? "border-red" : ""
+                      }`}
+                    >
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="Support Request" />
+                        </FormControl>
+                        <FormLabel className="font-normal text-black text-base cursor-pointer">
+                          Support Request
+                        </FormLabel>
+                      </FormItem>
+                    </div>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel className="text-black text-base cursor-pointer">
+                  Message *
+                </FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Type your message here."
+                    className={`resize-none h-52 ${
+                      fieldState.invalid ? "border-red" : ""
+                    }`}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="consent"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+
+                <div className="space-y-1 leading-none">
+                  <FormLabel className="text-black cursor-pointer">
+                    I consent to being contacted by the team *
+                  </FormLabel>
+                  <FormMessage />
+                </div>
+              </FormItem>
+            )}
+          />
+          <Button className="w-full bg-green-600 hover:bg-green-600 text-base font-bold">
+            Submit
+          </Button>
+        </form>
+      </Form>
+      <ToastContainer
+        toastClassName={() =>
+          "relative flex  p-4 rounded-md bg-green-600 text-white text-lg w-[440px]"
+        }
+      />
+    </>
   )
 }
